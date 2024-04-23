5f13f74f53ab71ebca65f93682628f7ebd4d4146
C4Kitakyushu <156558807+C4Kitakyushu@users.noreply.github.com>
2024-04-23 23:14:41 +0800

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,15 @@ module.exports = {
                         confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
                         successGlobal: "Changed prefix of system bot to: %1",
                         successThisThread: "Changed prefix in your box chat to: %1",
-                        myPrefix: "𝗛𝗘𝗟𝗟𝗢 𝗨𝗦𝗘𝗥 𝗜'𝗠 𝗚𝗘𝗜𝗦𝗛𝗔 𝗔𝗡𝗗 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 ➪:%1\n𝗖𝗛𝗔𝗧➪:%2𝗛𝗘𝗟𝗣 \n𝗧𝗢 𝗦𝗘𝗘 𝗔𝗟𝗟 𝗔𝗩𝗔𝗜𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦♡︎"
+                        myPrefix: "𝗛𝗘𝗟𝗟𝗢 𝗨𝗦𝗘𝗥 𝗜'𝗠 𝗔𝗖𝗧𝗜𝗩𝗘 𝗥𝗜𝗚𝗛𝗧 𝗡𝗢𝗪!! 𝗕𝗧𝗪 𝗜'𝗠 𝗔𝗡𝗦𝗘𝗟 𝗔𝗡𝗗 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 ➪: %1\n𝗖𝗛𝗔𝗧➪:  %2𝗛𝗘𝗟𝗣 \n𝗧𝗢 𝗦𝗘𝗘 𝗔𝗟𝗟 𝗔𝗩𝗔𝗜𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦♡︎\n\𝗇
+
+     ⠀⠀⠀⠀⠀／＞    フ
+ 　　　　　| 　_　 _ |
+ 　 　　　／` ミ＿xノ
+ 　　 　 /　　　 　 |
+ 　　　 /　 ヽ　　 ﾉ
+ 　 　 │　　| | |
+𝖫𝖠𝖬𝖡𝖨𝖭𝖦𝖨𝖭 𝖬𝖮 𝖭𝖠𝖬𝖠𝖭 𝖠𝖪𝖮😣"
                 }
         },
 
